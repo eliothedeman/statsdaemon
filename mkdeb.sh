08793acbe22aaa3c665195580c92df43ec5cdbf7
eliothedeman <eliot.d.hedeman@gmail.com>
2015-04-03 11:40:36 -0400

    changed deb package name from statsd-go to statsd

--- mkdeb.sh
+++ mkdeb.sh
@@ -5,7 +5,7 @@ mkdir -p etc/statsd
 mv statsd opt/statsd/statsd
 
 # build the debian package
-fpm -s dir -t deb --name statsd-go -v $(date +%s).0 etc opt
+fpm -s dir -t deb --name statsd -v $(date +%s).0 etc opt
 
 # clean up
 rm -r opt etc
